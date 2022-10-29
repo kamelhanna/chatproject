214b0adbcb6ccaf5934422e2f784ddcab4d64763
Kamel Hanna <kamelabdelmessih74@gmail.com>
2022-10-29 13:34:14 +0200

    Add websocket and frontend

--- src/main/java/com/iti/chatproject/service/UserService.java
+++ src/main/java/com/iti/chatproject/service/UserService.java
@@ -2,6 +2,8 @@ package com.iti.chatproject.service;
 
 import com.iti.chatproject.entity.User;
 import com.iti.chatproject.exception.UserNotFoundException;
+import com.iti.chatproject.mapstruct.dto.UserDto;
+import com.iti.chatproject.mapstruct.mapper.MapStructMapper;
 import com.iti.chatproject.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +13,16 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserService {
     @Autowired
     private UserEntityRepository userEntityRepository;
+    @Autowired
+    private MapStructMapper mapstructMapper;
 
     /*public User getUser(){
         return userEntityRepository.save(User.builder().userEmail("kamel")
                 .userPassword("Kamel").userLogin("Kamel").build());
     }*/
 
-    public User addUser(User user) {
-        return userEntityRepository.save(user);
+    public UserDto addUser(UserDto userDto) {
+        return mapstructMapper.userToUserDto(userEntityRepository.save(mapstructMapper.userDtoToUser(userDto)));
     }
 
     public User updateUser(User user){
